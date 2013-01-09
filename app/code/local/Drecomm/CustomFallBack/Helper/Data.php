7beb7bd8cbd49a4e83c5a118db4a8198dd7e5e3b
Armand Ndizigiye <a.ndizigiye@drecomm.nl>
2013-01-09 17:13:52 +0100

    first commit

---
+++ app/code/local/Drecomm/CustomFallBack/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+
+class Drecomm_CustomFallback_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ app/code/local/Drecomm/CustomFallBack/Model/Design/Package.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Overrides the class Mage_Core_Model_Design_Package
+ * @category    Drecomm
+ * @package     Drecomm_CustomFallBack
+ * @author      Drecomm
+ */
+class Drecomm_CustomFallback_Model_Design_Package extends Mage_Core_Model_Design_Package {
+
+    /**
+     * Override the _fallback method
+     * @param string $file
+     * @param array &$params
+     * @param array $fallbackScheme
+     * @return string
+     */
+    protected function _fallback($file, array &$params, array $fallbackScheme = array(array())) {
+
+        $fallback = trim(Mage::getStoreConfig('drecomm_costumfallback/general/fallback'));
+        $themes = split(',',$fallback);
+
+        if (!empty($themes)){
+            foreach( $themes as $theme){
+                $custom = array('_theme' => $theme);
+                array_push($fallbackScheme ,$custom);
+            }
+        }
+
+        if ($this->_shouldFallback) {
+            foreach ($fallbackScheme as $try) {
+                $params = array_merge($params, $try);
+                $filename = $this->validateFile($file, $params);
+                if ($filename) {
+                    return $filename;
+                }
+            }
+            $params['_package'] = self::BASE_PACKAGE;
+            $params['_theme']   = self::DEFAULT_THEME;
+        }
+        return $this->_renderFilename($file, $params);
+    }
+}
+?>
